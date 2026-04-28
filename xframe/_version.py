2778fda904b2424af93f977477a5b3f1c493a5a5
R Watanabe <tetraliane999@gmail.com>
2026-04-28 16:10:43 +0900

    Stop automatic versioning
    setuptools_scm is not available in SACLA HPC

---
+++ xframe/_version.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+__all__ = [
+    "__version__",
+    "__version_tuple__",
+    "version",
+    "version_tuple",
+]
+
+version = __version__ = "0.9.9"
+version_tuple = __version_tuple__ = (0, 9, 9)
